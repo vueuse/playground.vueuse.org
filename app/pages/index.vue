1ba556a06fde5839ad2cc8d7e456c3a83ef036ea
cosine7 <48633709+cosine7@users.noreply.github.com>
2025-11-16 22:12:01 +0800

    fix: use +esm for vueuse >= 13 (#20)
    * fix: playground not working for alpha releases
    
    * fix: use regex to replace ./
    
    * fix: use package.json module field
    
    * chore: hardcode @vueuse/core when fetch
    
    * refactor: use +esm for vueuse >= 13
    
    * refactor: use semver to replace parseInt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,7 @@
 import type { OutputModes } from '@vue/repl'
 import type { ShallowRef } from 'vue'
 import { mergeImportMap, useStore, useVueImportMap } from '@vue/repl'
+import semver from 'semver'
 
 const showOutput = useRouteQuery<string, boolean>('showOutput', 'false', {
   transform: stringToBooleanTransformer,
@@ -40,8 +41,10 @@ const vueUsePackages = [
 ]
 
 function generateVueUseImportCDNs() {
+  const version = semver.coerce(vueuseVersion.value)
+  const file = vueuseVersion.value === 'latest' || (version && semver.gte(version, '13.0.0')) ? '+esm' : 'index.mjs'
   return vueUsePackages.map((p) => {
-    return [p, `https://cdn.jsdelivr.net/npm/${p}@${vueuseVersion.value}/index.mjs`]
+    return [p, `https://cdn.jsdelivr.net/npm/${p}@${vueuseVersion.value}/${file}`]
   })
 }
 
